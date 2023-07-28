99aac0b2193a5d1798be7c6254bf9941ba9013d5
Cunuduh <nickleodean.email3@gmail.com>
2023-07-27 23:11:10 -0400

    cool

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,32 @@
 import hljs from "highlight.js";
 
-const textarea = document.getElementById("code") || document.createElement("textarea")
-const langInput = document.getElementById("language")
-const button = document.getElementById("highlight")
+const textarea = document.getElementById("code") as HTMLTextAreaElement
+const langInput = document.getElementById("language") as HTMLInputElement
+const button = document.getElementById("highlight") as HTMLInputElement
 
 button.addEventListener("click", (evt) => {
     evt.preventDefault()
     const lang = langInput.value
     const code = textarea.value
-    const highlighted = highlight(code, lang)
+    const highlighted = highlight(code, lang) || code
 
     textarea.value = highlighted
 }, false)
 
-function highlight(str, lang) {
+function highlight(str: string, lang: string) {
     if (lang && hljs.getLanguage(lang)) {
-        try {
-            let out = hljs.highlight(lang, str).value
-            return unescape(format(out))
-        } catch (__) {}
+        let out = hljs.highlight(lang, str).value
+        return unescape(format(out))
     }
     return ""
 }
-function format(str) {
+function format(str: string) {
     str = str.replace(/ {2,}/g, "@@$&@@")
     .replace(/--/g, "@@--@@")
     .replace(/<\/span>/g, "[[/span]]")
     .replace(/hljs/g, "--hl")
     .replace(/<span class="(.*?)">/g, '[[span style="color:var($1)"]]')
-    .replace(/\[\[span style="color:var\((.*?)\)"]]/g, (match, p1) => {
+    .replace(/\[\[span style="color:var\((.*?)\)"]]/g, (match: string, p1: string) => {
         const part = p1.split(/\s(.+)/)[0].replace(/_/g, "")
         return `[[span style="color:var(${part})"]]`
     })
@@ -36,7 +34,7 @@ function format(str) {
     .replace(/var\(--hl-subst\)|var\(--hl-params\)/g, "initial")
     return str
 }
-function unescape(str) {
+function unescape(str: string) {
     let doc = new DOMParser().parseFromString(str, "text/html")
     return doc.documentElement.textContent
 }
